84dfea2ebbf42bc908a3754bfe0ba67ff92756c6
Sophie Engle <sjengle@cs.usfca.edu>
2024-01-19 22:29:07 -0800

    Adding exception demo template

---
+++ src/main/templates/edu/usfca/cs272/template/exceptions/SimpleGrader.java
@@ -0,0 +1,31 @@
+package edu.usfca.cs272.template.exceptions;
+
+import java.util.Scanner;
+
+public class SimpleGrader {
+	public static int calcPercentage(int earned, int possible) {
+		return 100 * earned / possible;
+	}
+
+	public static void printResult(int earned, int possible, int percentage) {
+		System.out.printf("%d/%d = %d%% %n", earned, possible, percentage);
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter total points earned: ");
+		int earned = scanner.nextInt();
+
+		System.out.print("Enter total points possible: ");
+		int possible = scanner.nextInt();
+
+		int percentage = calcPercentage(earned, possible);
+		printResult(earned, possible, percentage);
+
+		scanner.close();
+		System.out.println("[done]");
+	}
+
+	private SimpleGrader() {}
+}
