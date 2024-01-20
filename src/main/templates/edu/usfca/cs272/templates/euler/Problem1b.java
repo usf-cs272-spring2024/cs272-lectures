fc2f311355663584b09e77b6227f86d52e1a7d21
Sophie Engle <sjengle@cs.usfca.edu>
2024-01-19 22:33:02 -0800

    Adding Project Euler templates

---
+++ src/main/templates/edu/usfca/cs272/templates/euler/Problem1b.java
@@ -0,0 +1,14 @@
+package edu.usfca.cs272.templates.euler;
+
+public class Problem1b {
+	public static int sumMultiples(int max) {
+		// TODO Fill in solution
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+
+	public static void main(String[] args) {
+		System.out.println(sumMultiples(1000));
+	}
+
+	private Problem1b() {}
+}
