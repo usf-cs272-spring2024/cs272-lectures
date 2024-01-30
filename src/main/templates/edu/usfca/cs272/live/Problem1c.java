90e705a4f10320858e56b56d06e155988ba62058
Sophie Engle <sjengle@cs.usfca.edu>
2024-01-30 11:00:21 -0800

    Adding ProjectEuler live demos

---
+++ src/main/templates/edu/usfca/cs272/live/Problem1c.java
@@ -0,0 +1,15 @@
+package edu.usfca.cs272.live;
+
+public class Problem1c {
+	public static int inputNonNegativeInteger() {
+		// TODO Fill in solution
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+
+	public static void main(String[] args) {
+		// TODO Fill in solution
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+
+	private Problem1c() {}
+}
