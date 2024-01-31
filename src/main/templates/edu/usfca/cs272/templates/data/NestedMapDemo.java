daec4948016e77899440f23f3a983764636f0869
Sophie Engle <sjengle@cs.usfca.edu>
2024-01-31 10:41:26 -0800

    Adding live code templates for data structure demos

---
+++ src/main/templates/edu/usfca/cs272/templates/data/NestedMapDemo.java
@@ -0,0 +1,16 @@
+package edu.usfca.cs272.templates.data;
+
+public class NestedMapDemo {
+
+	public static final String text = "time flies like an arrow fruit flies like a banana";
+
+	// 1, a
+	// 2, an
+	// 4, time, like
+	// 5, flies, arrow, fruit
+	// 6, banana
+
+	public static void main(String[] args) {
+		// TODO
+	}
+}
